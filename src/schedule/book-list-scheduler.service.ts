d0c9a79c6ed14c83f664557721b2a2446156fe7c
김만규 <123226237+fierceCry@users.noreply.github.com>
2024-09-16 02:30:00 +0900

    feat: BookListSchedulerService에 재고 업데이트 로직 추가
    - 재고가 10개 미만인 경우 10개로 업데이트하는 로직 구현
    - 매일 오후 12시에 재고를 업데이트하도록 스케줄 설정
    - 재고가 업데이트되었는지 여부를 나타내는 로그 추가
    - 재고 업데이트 과정에서의 오류 처리 개선

--- src/schedule/book-list-scheduler.service.ts
+++ src/schedule/book-list-scheduler.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { BookListSeeder } from 'src/database/seeds/01.book.seed';
 import { DataSource } from 'typeorm';
+import { BooksEntity } from '../../src/entities/books.entity'; // Book 엔티티를 가져옵니다.
 
 @Injectable()
 export class BookListSchedulerService {
@@ -9,14 +10,49 @@ export class BookListSchedulerService {
 
   constructor(private dataSource: DataSource) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  @Cron(CronExpression.EVERY_DAY_AT_NOON) // 
   // @Cron('*/5 * * * *') // 5분마다 실행
   async handleCron() {
     const seeder = new BookListSeeder();
+
     try {
-      await seeder.run(this.dataSource, null); 
+      // 알라딘 API에서 데이터를 가져오는 작업
+      await seeder.run(this.dataSource, null);
+
+      // 재고 업데이트 작업
+      await this.updateBookStock();
     } catch (error) {
       this.logger.error('알라딘 API 실행 중 오류 발생:', error);
     }
   }
+
+  private async updateBookStock() {
+    const queryRunner = this.dataSource.createQueryRunner();
+  
+    try {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+  
+        const result = await queryRunner.manager
+        .createQueryBuilder()
+        .update(BooksEntity)
+        .set({ stockQuantity: 10 })
+        .where("stockQuantity < 10") // 재고가 10개 미만인 경우에만 업데이트
+        .execute();
+  
+      await queryRunner.commitTransaction();
+  
+      if (result.affected && result.affected > 0) {
+        this.logger.log('재고가 10개로 업데이트되었습니다.');
+      } else {
+        this.logger.log('업데이트할 재고가 없습니다.');
+      }
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      this.logger.error('재고 업데이트 중 오류 발생:', error);
+    } finally {
+      await queryRunner.release();
+    }
+  }
+  
 }
